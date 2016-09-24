655458ee1ebc9d0515a1595f76f7b58b11adb8ad
Frank Paschen <frankpaschen99@gmail.com>
2016-09-24 18:07:53 -0400

    complete cancer

--- core/src/utilities/Fragment.java
+++ core/src/utilities/Fragment.java
@@ -6,15 +6,26 @@ import java.util.List;
 import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
 
-/* The Fragment class represents polygons on the Map that have unique properties */
+/* The Fragment class represents polygons on the Map that have unique physical properties */
 
 public class Fragment {
 	private Polygon p;
 	private byte regionId;
 	
-	public Fragment(byte i, List<Vector2> _vertices) {
-		// TODO: be able to create a Polygon object from _vertices
-		
+	/* WHAT THE HELL IS THIS CANCER */
+	public Fragment(byte _regionId, List<Vector2> _vertices) {
+		float[] vertices = new float[_vertices.size()*2];
+		List<Float> v = new ArrayList<Float>();
+		for (Vector2 k : _vertices) {
+			v.add(k.x);
+			v.add(k.y);
+		}
+		int i = 0;
+		for (Float f : v) {
+			vertices[i++] = (f != null ? f : Float.NaN);
+		}
+		this.p = new Polygon(vertices);
+		this.regionId = regionId;
 	}
 	public Polygon getArea() {
 		return this.p;
